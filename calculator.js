c46d6ec4c012c488105ec5fc8877fe106f01efd0
Anton <anton_broos@outlook.com>
2020-02-05 23:02:29 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,12 @@
+exports.Som = function(n1, n2){
+    return n1+n2;
+};
+exports.Verschil = function(n1, n2){
+    return n1-n2;
+};
+exports.Product = function(n1, n2){
+    return n1*n2;
+};
+exports.Quotiënt = function(n1, n2){
+    return n1/n2;
+};
